26c0cc638e419acc74c41fcc9e7c0ccd24566383
lafiti duarte <lafitivianaoliveiraduarte@gmail.com>
2024-03-15 22:43:30 -0300

    add basic functions

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const visor = document.querySelector('.visor')
-console.log(visor)
+const visor = document.querySelector('p')
+const num = document.querySelectorAll('.btn')
+const c = document.querySelector('.c')
+
+function clicked(param){
+   visor.innerHTML += param
+}
+function limpa(){
+    visor.innerHTML = ''
+    console.log('aqui')
+}
+function resultado(){
+    conta = Number(visor.innerHTML)
+    console.log(typeof conta)
+}
